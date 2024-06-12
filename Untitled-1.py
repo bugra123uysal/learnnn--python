e150da5bdcd8f1b99ee73e0e73490b5f3125d842
buğra uysal <uysalbugra134@gmail.com>
2024-06-12 03:54:07 +0300

    learn

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,26 @@
-print("hello world")
+
+
+
+x="merhaba"
+
+def gg():
+    
+    x="doğum günü"
+    print(x)
+
+gg()
+
+print(x)
+
+
+
+tt=("at", "avrat", "silah")
+print(tt)
+
+
+x= 10
+print(type(x))
+
+
+v="tip"
+print(type(v))
